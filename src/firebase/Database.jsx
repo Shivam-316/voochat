f96f953962777bc7cbcbfa25b56e5ec55c5c8653
Shivam Sharma <56474719+Shivam-316@users.noreply.github.com>
2022-10-09 21:31:12 +0530

    Disconnected Emulators

--- src/firebase/Database.jsx
+++ src/firebase/Database.jsx
@@ -15,7 +15,7 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 const auth = getAuth(app);
-// const storage = getStorage(app);
+const storage = getStorage(app);
 // connectFirestoreEmulator(db, 'localhost', 8080);
 // connectAuthEmulator(auth, "http://localhost:9099");
 // connectStorageEmulator(storage, "localhost", 9199);
