487d80c4267cb3478aab9cc731277a3d9eeed2d5
Shivam Sharma <56474719+Shivam-316@users.noreply.github.com>
2022-11-09 23:11:06 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,11 @@
 import { defineConfig } from 'vite'
-import path from 'path'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
 })
+
+export {
+  vite_config_default as default
+};
