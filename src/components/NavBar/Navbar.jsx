1d1052ca68b7478944e995f7ec52ed100b19ae87
Shivam Sharma <56474719+Shivam-316@users.noreply.github.com>
2022-11-14 18:39:27 +0530

    UI update

--- src/components/NavBar/Navbar.jsx
+++ src/components/NavBar/Navbar.jsx
@@ -1,18 +1,18 @@
-import React, { useState } from "react";
+// import React, { useState } from "react";
 import { Signout } from "../Logout/Signout";
 import Logo from "../../assets/logo.svg";
-import Button from "../StyledButtons/Button.jsx";
+// import Button from "../StyledButtons/Button.jsx";
 import "./navbar.css";
 import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
 export const Navbar = () => {
   const currentUser = useSelector((state) => state.userData.user);
-  const [darkTheme, setDarkTheme] = useState(true);
+  // const [darkTheme, setDarkTheme] = useState(true);
 
-  const toggleTheme = () => {
-    document.documentElement.classList.toggle("dark");
-    setDarkTheme((isdarkTheme) => !isdarkTheme);
-  };
+  // const toggleTheme = () => {
+  //   document.documentElement.classList.toggle("dark");
+  //   setDarkTheme((isdarkTheme) => !isdarkTheme);
+  // };
   return (
     <div className="nav">
       <Link to="/">
@@ -22,14 +22,14 @@ export const Navbar = () => {
       </Link>
       <div className="nav__buttons">
         {currentUser && <Signout />}
-        <Button onClick={toggleTheme}>
+        {/* <Button onClick={toggleTheme}>
           {darkTheme ? (
             <i className="fa-sharp fa-solid fa-moon" />
           ) : (
             <i className="fa-sharp fa-solid fa-sun" />
           )}{" "}
           <span style={{ fontSize: "0.5rem" }}>beta</span>
-        </Button>
+        </Button> */}
       </div>
     </div>
   );
